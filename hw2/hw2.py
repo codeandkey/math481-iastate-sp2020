de3299fa1aab642f23d46cfb2f9efd31da14d06a
codeandkey <jtst@iastate.edu>
2020-02-03 16:48:47 -0600

    hw2: initial commit

---
+++ hw2/hw2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# MATH 481 - HW2 source code.
+# Justin Stanley
+# Feb 4, 2020
+
+import sys
+import matplotlib
+import numpy
+
+def main():
+    print('Nothing here.')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
